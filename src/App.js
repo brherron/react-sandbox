82a5d78445d23efdb9e3c423c90d5ea7432ed047
brherron <brherron@wisc.edu>
2022-07-15 09:28:42 -0500

    Finished custom 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import UseCallbackExample from "./components/UseCallbackExample";
-import UseMemoExample from "./components/UseMemoExample";
-import UseRefExample1 from "./components/UseRefExample1";
-import UseRefExample2 from "./components/UseRefExample2";
-import UseRefExample3 from "./components/UseRefExample3";
+// import UseCallbackExample from "./components/UseCallbackExample";
+// import UseMemoExample from "./components/UseMemoExample";
+// import UseRefExample1 from "./components/UseRefExample1";
+// import UseRefExample2 from "./components/UseRefExample2";
+// import UseRefExample3 from "./components/UseRefExample3";
+
+import CustomHookExample1 from "./components/CustomHookExample1";
 
 function App() {
   return (
     <div className="container mt-5">
-      <UseCallbackExample />
+      <CustomHookExample1 />
     </div>
   );
 }
---
+++ src/components/CustomHookExample1.jsx
@@ -0,0 +1,23 @@
+import useFetch from '../hooks/useFetch'
+
+function CustomHookExample1() {
+  const { data, loading, error } = useFetch('https://jsonplaceholder.typicode.com/posts', { contentType: 'application/json'})
+
+  if (error) {
+    console.log(error)
+  }
+
+  if (loading) {
+    return <h3>Loading...</h3>
+  }
+
+  return (
+    <div>
+      {data.map(post => (
+        <h3 key={post.id}>{post.title}</h3>
+      ))}
+    </div>
+  )
+}
+
+export default CustomHookExample1
